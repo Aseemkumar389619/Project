02d46618fcde7144fc964fb621380d4c13ee355f
edusofttanjinliza <tanjinliza3934@gmail.com>
2019-09-12 15:32:15 +0600

    New desing

---
+++ MyWin/MyWin/Home.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace MyWin
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showButton = new System.Windows.Forms.Button();
+            this.name = new System.Windows.Forms.Label();
+            this.showLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(310, 46);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 0;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Location = new System.Drawing.Point(55, 49);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(33, 13);
+            this.name.TabIndex = 1;
+            this.name.Text = "name";
+            // 
+            // showLabel
+            // 
+            this.showLabel.AutoSize = true;
+            this.showLabel.Location = new System.Drawing.Point(205, 116);
+            this.showLabel.Name = "showLabel";
+            this.showLabel.Size = new System.Drawing.Size(0, 13);
+            this.showLabel.TabIndex = 2;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(151, 46);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 3;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.showLabel);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.showButton);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.Home_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Label showLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+    }
+}
+
