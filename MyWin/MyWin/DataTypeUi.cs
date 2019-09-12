08a59d3004bdb1379eb11009c341ce45676de18f
AseemKumar389619 <aseemkumar389619@gmail.com>
2019-09-12 17:11:35 +0600

    New Work

---
+++ MyWin/MyWin/DataTypeUi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyWin
+{
+    public partial class DataTypeUi : Form
+    {
+        public DataTypeUi()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            if(itemComboBox.Text == "")
+            {
+                MessageBox.Show("select an item");
+            }
+            else
+            {
+                MessageBox.Show(itemComboBox.Text + " is selected");
+            }
+        }
+    }
+}
--- MyWin/MyWin/Program.cs
+++ MyWin/MyWin/Program.cs
@@ -16,7 +16,11 @@ namespace MyWin
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Home());
+
+
+            //Application.Run(new Home());
+            Application.Run(new DataTypeUi());
+
         }
     }
 }
